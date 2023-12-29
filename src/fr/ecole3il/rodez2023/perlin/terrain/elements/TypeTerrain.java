58a847a9b864e2e546349c18d588dc013ae6beea
EnzoFavret3il <favrete@3il.fr>
2023-12-29 15:22:09 +0100

    Temp Update on TypeTerrain

--- src/fr/ecole3il/rodez2023/perlin/terrain/elements/TypeTerrain.java
+++ src/fr/ecole3il/rodez2023/perlin/terrain/elements/TypeTerrain.java
@@ -1,10 +1,24 @@
 package fr.ecole3il.rodez2023.perlin.terrain.elements;
 
+import java.awt.image.BufferedImage;
+
 import fr.ecole3il.rodez2023.perlin.Utils;
 
 public enum TypeTerrain {
+	Coniferous_forest("coniferouse_forest.png"),
+	Deciduous_forest("deciduous_forest.png"),
+	Desert("desert.png"),
+	Hills("hills.png"),
+	Mountain("mountain.png"),
+	Ocean("ocean.png"),
+	Plain("plain.png"),
+	Tundra("tundra.png");
+	
+	 private final String nomfichier;
 
-	private String nomfichier;
+	 private TypeTerrain(String nomfichier) {
+		 this.nomfichier=nomfichier;
+	 }
 	public BufferedImage getImage() {
 		return Utils.chargerTuile(nomfichier);
 	}
