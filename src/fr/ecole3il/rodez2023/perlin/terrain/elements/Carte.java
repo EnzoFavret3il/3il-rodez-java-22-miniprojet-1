66f6760f1eab8fc2f9fe552498e66c7197c5edbd
EnzoFavret3il <favrete@3il.fr>
2024-01-22 18:52:57 +0100

    Création des attributs de la classe Carte

--- src/fr/ecole3il/rodez2023/perlin/terrain/elements/Carte.java
+++ src/fr/ecole3il/rodez2023/perlin/terrain/elements/Carte.java
@@ -1,5 +1,10 @@
 package fr.ecole3il.rodez2023.perlin.terrain.elements;
 
 public class Carte {
-
+	private String nom;
+	private double largeur;
+	private double hauteur;
+	private Terrain tab;
+	
+	
 }
